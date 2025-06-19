c23ecb5f84bc147ebc0e2adf06d8cf2dad724f4e
882Eunae <pubao47@gmail.com>
2025-06-20 01:16:19 +0900

    정은애/주문상세조회/2025.06.19

--- dadam/src/main/java/com/dadam/sales/order/service/OrdDtlVO.java
+++ dadam/src/main/java/com/dadam/sales/order/service/OrdDtlVO.java
@@ -5,13 +5,13 @@ import lombok.Data;
 @Data
 public class OrdDtlVO {
 	//주문건 상세조회 
-	private String ordDtlCode;
-	private String itemCode;
-	private String name; 
-	private int price;
-	private int quantity;
-	private int supPrice; 
-	private int vatPrice;
-	private int totPrice; 
-	private int discPrice;
+	private String ordDtlCode;//상세정보 
+	private String itemCode; //품목정보
+	private String name; //이름
+	private int price; //가격
+	private int quantity; //수량
+	private int supPrice; //공급가액
+	private int vatPrice; //부가세
+	private int totPrice; //총가격 
+	private int discPrice; //할인금액 
 }
