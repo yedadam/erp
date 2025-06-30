6cf86191a1c421c6e4036b7d6f3f744a678d19f7
882Eunae <pubao47@gmail.com>
2025-07-01 04:06:37 +0900

    ddd

--- dadam/src/main/java/com/dadam/sales/shipreq/mapper/ShipreqMapper.java
+++ dadam/src/main/java/com/dadam/sales/shipreq/mapper/ShipreqMapper.java
@@ -8,4 +8,5 @@ import com.dadam.sales.shipreq.service.ShipReqVO;
 public interface ShipreqMapper {
 	public List<ShipReqVO> findShipreqList(); //출하의뢰 리스트조회
 	public List<ShipReqDtlVO> findShipreqDtlList(String shipReqCode); //출하의뢰코드로 상세조회
+	
 }
