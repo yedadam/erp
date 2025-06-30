6ddcfa9734cb2a776ff1a8d31de8637b3642c56d
Lee Byungchan <a_astory@naver.com>
2025-06-30 17:57:17 +0900

    병찬/전표 등록중 / 2025.06.30

--- dadam/src/main/java/com/dadam/acc/account/web/ChitRestController.java
+++ dadam/src/main/java/com/dadam/acc/account/web/ChitRestController.java
@@ -1,10 +1,14 @@
 package com.dadam.acc.account.web;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dadam.acc.account.service.ChitService;
@@ -21,7 +25,26 @@ public class ChitRestController {
 		return chitService.chitFindAll();
 	}
 	
+	@GetMapping("/acctCodeFind")
+    public List<Map<String, String>> acctCodeFind(@RequestParam("keyword") String keyword) {
+        return chitService.acctCodeFindByCode(keyword);
+    }
 	
+	@GetMapping("/indTypeFindByCode")
+	public List<Map<String, String>> indTypeFindByCode(@RequestParam("keyword") String keyword){
+		return chitService.indTypeFindByCode(keyword);
+	}
 	
+	@GetMapping("/chitTypeFindByCode")
+	public List<Map<String, String>> chitTypeFindByCode(@RequestParam("keyword") String keyword){
+		return chitService.chitTypeFindByCode(keyword);
+	}
+	
+	@PostMapping("/saveAll")
+	 public void saveAll(@RequestBody ChitVO chit) {
+		System.out.println("👉 INSERT할 데이터: " + chit);
+		chitService.saveAll(chit);
+	}
+
 
 }
