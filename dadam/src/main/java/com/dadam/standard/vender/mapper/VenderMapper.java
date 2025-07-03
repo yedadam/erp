42e324b2e0069e4006d4d89ffddfb050304016e2
882Eunae <pubao47@gmail.com>
2025-07-03 17:48:16 +0900

    정은애/거래처관리/2025.07.03

--- dadam/src/main/java/com/dadam/standard/vender/mapper/VenderMapper.java
+++ dadam/src/main/java/com/dadam/standard/vender/mapper/VenderMapper.java
@@ -7,5 +7,8 @@ import org.apache.ibatis.annotations.Param;
 import com.dadam.standard.vender.service.VenderVO;
 
 public interface VenderMapper {
-	List<VenderVO> venderFindAll(@Param("comId") String comId,@Param("type") String type, @Param("value") String value); //거래처목록조회  
+	List<VenderVO> venderFindAll(@Param("comId") String comId,@Param("type") String type, @Param("value") String value); //거래처목록조회 
+	int insertVender(VenderVO vender); //거래처등록
+	int updateVender(VenderVO vender); //거래처정보수정 
+	int deleteVender(@Param("comId")String comId,@Param("vdrCode") String vdrCode); //거래처삭제하기
 }
