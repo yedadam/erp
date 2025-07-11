33fa85e25bc27f2ff2a4f5e30981487e814a17e5
hyunooki <aosdnr54321@gmail.com>
2025-07-11 10:34:04 +0900

    /신현욱/경로설정/2025.07.11

--- dadam/src/main/java/com/dadam/config/WebConfig.java
+++ dadam/src/main/java/com/dadam/config/WebConfig.java
@@ -1,21 +1,27 @@
 package com.dadam.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
     	registry.addResourceHandler("/uploads/**")
     		    .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
 
 		registry.addResourceHandler("/contracts/**")
-				.addResourceLocations("file:" + System.getProperty("user.dir") + "/contracts/");
+				.addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/contracts/");
 		registry.addResourceHandler("/signatures/**")
-		        .addResourceLocations("file:" + System.getProperty("user.dir") + "/signatures/");
+		        .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/signatures/");
 		registry.addResourceHandler("/settlementEle/**")
-        .addResourceLocations("file:" + System.getProperty("user.dir") + "/settlementEle/");
+        .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/settlementEle/");
+		
     }
+    
+    
 } 
