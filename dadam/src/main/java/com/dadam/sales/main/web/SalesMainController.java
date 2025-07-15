43a18957a2ed78c407c9bdd4b67be39ff2113c23
hyunooki <aosdnr54321@gmail.com>
2025-07-15 14:13:29 +0900

    /신현욱/전체레이아웃 수정/2025.07.15

--- dadam/src/main/java/com/dadam/sales/main/web/SalesMainController.java
+++ dadam/src/main/java/com/dadam/sales/main/web/SalesMainController.java
@@ -1,16 +1,33 @@
 package com.dadam.sales.main.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dadam.main.service.MainService;
+import com.dadam.main.service.MainVO;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RequestMapping("/erp")
 @Controller
 public class SalesMainController {
-	
+	@Autowired
+	MainService service;
 	@GetMapping("/sales")
-	public String test() {
-		return "sales/salesMain";
+	public String mainPage(HttpServletRequest request, Model model) {
+		List<Integer> result = service.today();
+		model.addAttribute("todayOrderPrice", result.get(0));
+		model.addAttribute("todayPurchasePrice", result.get(1));
+		model.addAttribute("todayPurchaseQunatity", result.get(2));
+		model.addAttribute("todayTranPrice", result.get(3));
+		List<MainVO> safeResult = service.safeList();
+		model.addAttribute("safeList", safeResult);
+	    return "home";
 	}
 	//@GetMapping("/quote")
 	//public String quote() {
